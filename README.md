db7f55821e7777e0607bc2d2292496bc799fa03f
Node Sans <node189@icloud.com>
2022-05-03 12:07:12 +0800

    Update makefile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # games-admin-server
 一个迫真亚运会志愿者管理系统后端
+
+## 部署准备
+
+- nohup 用来后台执行程序
+- make 用来调用 makefile
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start server gracefully
+	server := endless.NewServer(":8083", router)
+	server.BeforeBegin = func(add string) {
+		// Saving pid info
+		pid := os.Getpid()
+		log.Printf("Actual pid is %d", pid)
+
+		// TODO 这个地方的 ./dist 要使用配置文件来读取
+		file, _ := os.OpenFile("./dist/pid.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		defer file.Close()
+
+		writer := bufio.NewWriter(file)
+		_, _ = writer.WriteString(fmt.Sprintf("%v", pid))
+		_ = writer.Flush()
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
 }
