645b05b57dbf5f4a8b1891ee6edb9434c319e85f
Node Sans <node189@icloud.com>
2022-05-05 00:35:04 +0800

    添加配置文件路径参数

--- global/flags.go
+++ global/flags.go
@@ -8,6 +8,7 @@ import "flag"
 
 // daemonMode run server in daemon mode
 var DaemonMode = flag.Bool("d", false, "Run server in daemon mode")
+var ConfigPath = flag.String("config", "./config.yml", "The config file path")
 
 func InitFlag() {
 	// init flags
