001423c9c6679418a17bd02c3050e9e6c7bf9459
IInfo <info@i1nfo.com>
2022-05-04 15:09:18 +0800

    update

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
+	"log"
+	"os"
 )
 
+// define runtime args
+var daemon = flag.Bool("d", false, "Run in daemon mode")
+
 func main() {
+	// Parse args
+	flag.Parse()
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -20,19 +26,32 @@ func main() {
 
 	// Start server gracefully
 	server := endless.NewServer(":8083", router)
-	server.BeforeBegin = func(add string) {
-		// Saving pid info
-		pid := os.Getpid()
-		log.Printf("Actual pid is %d", pid)
 
-		// TODO 这个地方的 ./dist 要使用配置文件来读取
-		file, _ := os.OpenFile("./dist/pid.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-		defer file.Close()
+	// daemon mode
+	if *daemon {
+		server.BeforeBegin = func(add string) {
+			// Saving pid info
+			pid := os.Getpid()
+			log.Printf("Actual pid is %d", pid)
 
-		writer := bufio.NewWriter(file)
-		_, _ = writer.WriteString(fmt.Sprintf("%v", pid))
-		_ = writer.Flush()
+			// TODO 这个地方的 ./dist 要使用配置文件来读取
+			file, err := os.OpenFile("./dist/pid.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			defer func(file *os.File) {
+				err := file.Close()
+				if err != nil {
+					// TODO
+				}
+			}(file)
+
+			writer := bufio.NewWriter(file)
+			_, _ = writer.WriteString(fmt.Sprintf("%v", pid))
+			_ = writer.Flush()
+		}
 	}
+
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
