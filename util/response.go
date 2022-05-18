c6dae912d7fd4a83a4c7d7e790e8c7bfb7d50ab1
Node Sans <node189@icloud.com>
2022-05-18 09:06:58 +0800

    Add response util

---
+++ util/response.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func standardResp(context *gin.Context, code int, msg string, data interface{}) {
+	context.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": msg,
+		"data": data,
+	})
+}
+
+// SuccessResp the successful response
+func SuccessResp(context *gin.Context, data interface{}) {
+	standardResp(context, 200, "ok", data);
+}
+
+// FailedResp the failed response
+func FailedResp(context *gin.Context, code int, msg string) {
+	standardResp(context, code, msg, nil);
+}
+
+func ParamsErrResp(context *gin.Context) {
+	FailedResp(context, 4001, "parameter error!");
+}
