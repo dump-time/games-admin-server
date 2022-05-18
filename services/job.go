af8a6cfb6e4733c675f3f57104f79b9a421d062e
IInfo <info@i1nfo.com>
2022-05-18 22:58:48 +0800

    Fix Get jobs API return format

--- services/job.go
+++ services/job.go
@@ -4,15 +4,25 @@ import (
 	"database/sql"
 	"github.com/dump-time/games-admin-server/global"
 	"github.com/dump-time/games-admin-server/model"
+	"time"
 )
 
 func AddJob(job *model.Job) error {
 	return global.DB.Create(job).Error
 }
 
-func GetJobs(teamId sql.NullInt64) ([]model.Job, error) {
-	var results []model.Job
-	err := global.DB.Find(&results, model.Job{
+type Jobs []struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Name      string    `json:"name"`
+	Content   string    `json:"content"`
+	Location  string    `json:"location"`
+}
+
+func GetJobs(teamId sql.NullInt64) (Jobs, error) {
+	var results Jobs
+	err := global.DB.Model(model.Job{}).Find(&results, model.Job{
 		TeamID: teamId,
 	}).Error
 
