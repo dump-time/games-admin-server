15a8d9f78afcf42b94740a6a630efa2e53731c2e
IInfo <info@i1nfo.com>
2022-05-18 08:59:45 +0800

    fix typo & problems

--- model/init.go
+++ model/init.go
@@ -1,13 +1,19 @@
 package model
 
-import "github.com/dump-time/games-admin-server/global"
+import (
+	"github.com/dump-time/games-admin-server/global"
+	"github.com/dump-time/games-admin-server/log"
+)
 
 func init() {
-	global.DB.AutoMigrate(
+	err := global.DB.AutoMigrate(
 		&Admin{},
 		&Team{},
 		&TeamAdmin{},
 		&Volunteer{},
 		&Job{},
 	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
