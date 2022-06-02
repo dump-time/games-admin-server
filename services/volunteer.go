bd5e8ae942370e1c171a93b31d56e2094fde1b54
Node Sans <node189@icloud.com>
2022-06-02 08:50:13 +0800

    Remove debug info in list volunteer api

--- services/volunteer.go
+++ services/volunteer.go
@@ -18,7 +18,7 @@ func ListVolunteers(teamID sql.NullInt64, offset int, pageSize int) ([]model.Vol
 	var volunteers []model.Volunteer
 	var result *gorm.DB
 	if teamID.Valid {
-		result = global.DB.Debug().Where(
+		result = global.DB.Where(
 			"team_id = ? and id >= (?)", 
 			teamID, 
 			global.DB.Raw("SELECT id FROM volunteers WHERE team_id = ? and deleted_at IS NULL limit 1 OFFSET ?", 
@@ -28,7 +28,7 @@ func ListVolunteers(teamID sql.NullInt64, offset int, pageSize int) ([]model.Vol
 			Limit(pageSize).
 			Find(&volunteers)
 	} else {
-		result = global.DB.Debug().Where(
+		result = global.DB.Where(
 			"id >= (?)", 
 			global.DB.Raw("SELECT id FROM volunteers WHERE deleted_at IS NULL limit 1 OFFSET ?", 
 				offset)).
